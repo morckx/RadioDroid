8fb89bf6952554fcb3467f53e218127755de443d
Marc K <morckx@gmail.com>
2022-10-07 19:02:04 +0200

    Fix media browser service and enable android auto support

--- app/src/main/java/net/programmierecke/radiodroid2/RadioDroidApp.java
+++ app/src/main/java/net/programmierecke/radiodroid2/RadioDroidApp.java
@@ -37,6 +37,7 @@ public class RadioDroidApp extends MultiDexApplication {
     private HistoryManager historyManager;
     private FavouriteManager favouriteManager;
     private RecordingsManager recordingsManager;
+    private FallbackStationsManager fallbackStationsManager;
     private RadioAlarmManager alarmManager;
     private TvChannelManager tvChannelManager;
 
@@ -95,6 +96,7 @@ public class RadioDroidApp extends MultiDexApplication {
 
         historyManager = new HistoryManager(this);
         favouriteManager = new FavouriteManager(this);
+        fallbackStationsManager = new FallbackStationsManager(this);
         recordingsManager = new RecordingsManager();
         alarmManager = new RadioAlarmManager(this);
 
@@ -127,6 +129,10 @@ public class RadioDroidApp extends MultiDexApplication {
                 .addInterceptor(new UserAgentInterceptor("RadioDroid2/" + BuildConfig.VERSION_NAME));
 
         httpClient = builder.build();
+    }
+
+    public FallbackStationsManager getFallbackStationsManager() {
+        return fallbackStationsManager;
     }
 
     public HistoryManager getHistoryManager() {
